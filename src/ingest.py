6214796c71e89c96fb6e20fcd1a2865f2507ad19
zhikaichen99 <zhikaichen1999@gmail.com>
2023-07-29 20:21:56 -0400

    pdf to text function

--- src/ingest.py
+++ src/ingest.py
@@ -1,6 +1,4 @@
 from PyPDF2 import PdfReader
-from langchain.document_loaders import CSVLoader
-
 
 def pdf_to_text(pdf_docs):
     text = ""
@@ -10,9 +8,4 @@ def pdf_to_text(pdf_docs):
         # loop through the pages of the pdf and extract the text
         for page in pdf_reader.pages:
             text += page.extract_text()
-    return text
-
-def csv_to_text(csv_file):
-    loader = CSVLoader(file_path = csv_file)
-    text = loader.load()
     return text
